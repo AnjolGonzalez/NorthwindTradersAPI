e6ee677f84be3f7e8529e46fb946144974d52d9e
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-12-21 10:54:02 -0500

    Completion of 2

--- src/main/java/com/pluralsight/NorthwindTradersAPI/models/Store.java
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/models/Store.java
@@ -1,22 +1,27 @@
 package com.pluralsight.NorthwindTradersAPI.models;
 
-import java.util.ArrayList;
+import com.pluralsight.NorthwindTradersAPI.dao.ProductDao;
 
-public class Store {
-    ArrayList<Product> products;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.awt.AWTEventMulticaster.add;
+
+public class Store implements ProductDao {
+    List<Product> products = new ArrayList<>();
 
     public Store() {
-        this.products = new ArrayList<>();
-        Product product = new Product(1, "object", 1, 2.25);
-        Product product2 = new Product(2, "object", 1, 5.25);
-        Product product3 = new Product(3, "object", 1, 10.25);
-        Product product4 = new Product(4, "object", 1, 8.25);
-        products.add(product);
-        products.add(product2);
-        products.add(product3);
-        products.add(product4);
+        add(new Product(1 , "Object", 1, 2.25));
+        add(new Product(2, "object", 1, 5.25));
+        add(new Product(3, "object", 1, 10.25));
+        add(new Product(4, "object", 1, 8.25));
+
     }
-    public ArrayList<Product> displayProducts() {
+
+    private void add(Product product) { products.add(product);
+    }
+
+    public List<Product> displayProducts() {
         return this.products;
     }
 
@@ -26,6 +31,20 @@ public class Store {
                 return product;
             }
         }
+        return null;
+    }
+
+    @Override
+    public ArrayList<Product> getAll() {
+        ArrayList<Product> products1 = new ArrayList<>();
+        for (Product p : products) {
+            products1.add(p);
+        }
+        return products1;
+    }
+
+    @Override
+    public Category getByID(int id) {
         return null;
     }
 }
